fa1e5d97d4f10a1d1de41180c210333c1f074a9b
IkBenNederlandsBoomer <70447379+IkBenNederlandsBoomer@users.noreply.github.com>
2020-08-30 13:42:17 +0200

    init
    init

--- commands/intake.js
+++ commands/intake.js
@@ -2,7 +2,79 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    return message.channel.send('@🔰Staff🔰');
+    const categoryID = "749591852361187398";
+
+    var userName = message.author.username;
+    var userDiscriminator = message.author.discriminator;
+
+    var reportBestaat = false;
+
+    message.guild.channels.cache.forEach(channel => {
+
+        if (channel.name == userName.toLowerCase() + "-" + userDiscriminator) {
+            reportBestaat = true;
+
+            message.reply("Je hebt al een report");
+
+            return;
+        }
+
+    });
+
+    if (reportBestaat) return;
+
+    var reportEmbed = new discord.MessageEmbed()
+        .setTitle("Hallo " + message.author.username)
+        .setFooter(`${message.author} kijk in intake kanaal om te alles in te vullen!`);
+
+    message.channel.send(reportEmbed);
+
+    message.guild.channels.create(userName.toLowerCase() + "-" + userDiscriminator, { type: 'text' }).then(
+        (createdChannel) => {
+            createdChannel.setParent(categoryID).then(
+                (settedParent) => {
+
+                    settedParent.updateOverwrite(message.guild.roles.cache.find(x => x.name === '@everyone'), {
+                        SEND_MESSAGES: false,
+                        VIEW_CHANNEL: false
+                    });
+
+                    settedParent.updateOverwrite(message.author.id, {
+                        CREATE_INSTANT_INVITE: false,
+                        READ_MESSAGES: true,
+                        SEND_MESSAGES: true,
+                        ATTACH_FILES: true,
+                        CONNECT: true,
+                        ADD_REACTIONS: true,
+                        VIEW_CHANNEL: true,
+                        READ_MESSAGE_HISTORY: true
+
+                    });
+
+                    settedParent.updateOverwrite(message.guild.roles.cache.find(x => x.name === '🔰Staff🔰'), {
+                        CREATE_INSTANT_INVITE: false,
+                        READ_MESSAGES: true,
+                        SEND_MESSAGES: true,
+                        ATTACH_FILES: true,
+                        CONNECT: true,
+                        ADD_REACTIONS: true,
+                        VIEW_CHANNEL: true,
+                        READ_MESSAGE_HISTORY: true
+
+                    });
+
+                    var embedParent = new discord.MessageEmbed()
+                        .setTitle(`Hallo ${message.author.username} Vul het onderstaande bericht in!`)
+                        .setDescription("Naam(irl):\nNaam(IG):\nHeb je al naagedacht aan een baan in de stad:\nHeb je familie in de stad:\nWat wordt je functie in de stad:");
+
+                    settedParent.send(embedParent);
+
+                }
+            ).cache(err => {
+            message.channel.send("Er is iets mis gegaan!");
+            });
+        }
+    )
 
 }
 
