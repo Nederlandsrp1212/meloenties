21092e345326a0c45738477bab195bf96503bdab
IkBenNederlandsBoomer <70447379+IkBenNederlandsBoomer@users.noreply.github.com>
2020-08-30 13:56:19 +0200

    init
    init

--- commands/close.js
+++ commands/close.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    return message.channel.send(`Om een **TICKET** te sluiten doe je !closeticket \nOm een **REPORT** te sluiten doe je !closereport`);
+    return message.channel.send(`Om een **TICKET** te sluiten doe je !closeticket \nOm een **REPORT** te sluiten doe je !closereport \nOm een **INTAKE** te sluiten doe je !closeintake`);
 
 }
 
