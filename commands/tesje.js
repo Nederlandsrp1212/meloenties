e6e1cc5095ac5e249ed3b9bf33d0d678c7f9b838
IkBenNederlandsBoomer <70447379+IkBenNederlandsBoomer@users.noreply.github.com>
2020-09-01 22:56:05 +0200

    init
    init

---
+++ commands/tesje.js
@@ -0,0 +1,15 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    var intakeRole = message.guild.roles.cache.get('743838009224593449');
+    if (!intakeRole) return message.channel.send("De rol muted bestaat niet!");
+    
+    await (intakePerson.roles.add(intakeRole.id));
+    message.channel.send(`${intakePerson} is gemuted dus kan niet meer praten!`);
+
+}
+
+module.exports.help = {
+    name: "testje"
+}
