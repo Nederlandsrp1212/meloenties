fbcea14d1ae2242e0c0f7b718328e5e516e0b6bc
IkBenNederlandsBoomer <70447379+IkBenNederlandsBoomer@users.noreply.github.com>
2020-09-04 17:36:57 +0200

    init
    init

---
+++ commands/closesollicitatie.js
@@ -0,0 +1,24 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    const categoryID = "751462086319800361";
+    const categoryID = "751107506654281819";
+
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Jij kan dit niet doen!");
+
+    if (message.channel.parentID == categoryID) {
+        message.channel.delete();
+
+    } else {
+        message.channel.send("Gelieve dit commando te doen bij een intake!");
+    }
+
+    var reportChannel = message.member.guild.channels.cache.find(channel => channel.name === "🔰logs🔰");
+    if (!reportChannel) return message.reply("Kan het log kanaal niet vinden?");
+
+}
+
+module.exports.help = {
+    name: "closesollicitatie"
+}
