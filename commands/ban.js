8cfb9174af0daa3c691fec2ccdb286ba33f34967
IkBenNederlandsBoomer <70447379+IkBenNederlandsBoomer@users.noreply.github.com>
2020-09-23 22:52:16 +0200

    init
    init

--- commands/ban.js
+++ commands/ban.js
@@ -79,7 +79,7 @@ async function promptMessage(message, author, time, reactions) {
         await message.react(reaction);
     }
 
-    var filter = (reaction, user) => reaction.includes(reaction.emoji.name && user.id === author.id);
+    var filter = (reaction, user) => reaction.includes(reactions.emoji.name && user.id === author.id);
 
     return message.awaitReactions(filter, { max: 1, time: time }).then(collected => collected.first() && collected.first().emoji.name);
 
