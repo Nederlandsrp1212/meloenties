3771af230f1ab503585b493e4ec0a8e0b8a46742
IkBenNederlandsBoomer <70447379+IkBenNederlandsBoomer@users.noreply.github.com>
2020-10-21 19:40:31 +0200

    init
    init

--- commands/hallo.js
+++ commands/hallo.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    return message.reply(`Hallo (Doe !hgh om verder te praten!)`);
+    return message.reply(`Hallo (Doe !hgh om verder te praten!!)`);
 
 }
 
